32466c559f4dff4385630ab9d3ab86b61e47dafd
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-03 09:29:21 -0700

    recipe-card-added

--- src/pages/RecipesSection/Recipes/Recipes.jsx
+++ src/pages/RecipesSection/Recipes/Recipes.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { BeakerIcon, HeartIcon } from "@heroicons/react/24/solid";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const Recipes = ({ recipe }) => {
 	const { recipe_id, name, image, ratings, ingredients, method } = recipe;
 	// console.log(recipe_id, name, image, ratings, ingredients, method);
+
 	return (
 		<div>
-			<div className='card w-96 h-[950px] bg-base-100 shadow-xl'>
+			<div className='card w-96 h-[920px] bg-base-100 shadow-xl'>
 				<figure>
 					<img src={image} alt={name} className='h-60 w-full' />
 				</figure>
@@ -20,18 +23,28 @@ const Recipes = ({ recipe }) => {
 					</p>
 					<p>
 						<span className='font-semibold'>Cooking Method: </span>
-						{method.length < 600 ? (
+						{method.length < 500 ? (
 							method
 						) : (
 							<>
-								{method.slice(0, 600)}...
+								{method.slice(0, 500)}...
 								<p className='font-semibold text-red-400'>Load More</p>
 							</>
 						)}
 					</p>
-					<div className='card-actions'>
-						<button>
-							<HeartIcon className='h-6 w-6 text-red-400' />
+					<div className='card-actions flex items-center'>
+						<div className='flex items-center flex-grow'>
+							<Rating
+								style={{ maxWidth: 100 }}
+								value={ratings}
+								readOnly
+								key={ratings}
+							/>
+							<p className='ml-2 font-semibold'>{ratings}</p>
+						</div>
+						<button className='btn btn-outline btn-error'>
+							<p className='text-red-400 hover:text-white'>Like </p>
+							<HeartIcon className='h-6 w-6 text-red-400 ml-2 hover:text-white' />
 						</button>
 					</div>
 				</div>
--- src/pages/RecipesSection/RecipesContainer/RecipesContainer.jsx
+++ src/pages/RecipesSection/RecipesContainer/RecipesContainer.jsx
@@ -8,7 +8,7 @@ const RecipesContainer = ({ chef }) => {
 
 	console.log("chef info", recipes);
 	return (
-		<div className='grid grid-cols-3 gap-10'>
+		<div className='grid grid-cols-3 gap-10 mt-10'>
 			{recipes?.map((recipe) => (
 				<Recipes recipe={recipe} />
 			))}
